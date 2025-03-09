7f2cc44ba72bfcc71a6a82d7f7b245cc00126141
Khen Bo Kan <hyonbokan@gmail.com>
2025-03-09 21:57:23 +0900

    graphql gateway setup

--- gateway/graphql_gateway.py
+++ gateway/graphql_gateway.py
@@ -2,3 +2,35 @@ import asyncio
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from graphql import graphql_sync, build_schema
+
+# GraphQL schema
+graphql_schema = build_schema(
+    """
+    type Query {
+        getMarketData: String
+        getTransactionData: String
+        getAnalyticsData: String
+    }
+    """
+)
+
+def resolve_get_market_data():
+    return "market data service response"
+
+def resolve_get_transaction_data():
+    return "transaction service response"
+
+def resolve_get_analytics_data():
+    return "analytics service response"
+
+
+# mapping queries to resolvers
+query_resolvers = {
+    "getMarketData": resolve_get_market_data,
+    "getTransactionData": resolve_get_transaction_data,
+    "getAnalyticsData": resolve_get_analytics_data,
+}
+
+class GraphQLHander(BaseHTTPRequestHandler):
+    def do_POST(self):
+        pass
