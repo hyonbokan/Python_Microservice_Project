235e734aa2a5b1214d0a1756eec642e6d6063cf3
hyonbokan <hyonbokan@gmail.com>
2025-03-10 15:51:37 +0900

    db migration and db setup bug fixed

--- utils/config.py
+++ utils/config.py
@@ -5,18 +5,18 @@ load_dotenv()
 
 class Config:
     # Database Configuration
-    POSTGRES_DB = os.getenv("POSTGRES_DB", "market_data")
-    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "microservice_db")
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "microservice_user")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password123")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "postgres")
     POSTGRES_PORT = int(os.getenv("POSTGRES_PORT", 5432))
-
+    
     # Redis Configuration
-    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
-    REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
-    REDIS_DB = int(os.getenv("REDIS_DB", 0))
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = int(os.getenv("REDIS_PORT"))
+    REDIS_DB = int(os.getenv("REDIS_DB"))
 
     # RabbitMQ Configuration
-    RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE", "market_data_queue")
-    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "localhost")
-    RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT", 5672))
+    RABBITMQ_QUEUE = os.getenv("RABBITMQ_QUEUE")
+    RABBITMQ_HOST = os.getenv("RABBITMQ_HOST")
+    RABBITMQ_PORT = int(os.getenv("RABBITMQ_PORT"))
